71ad1453d27c1a6b230c6a4b8b5ae43bcc096b5e
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-25 15:42:07 -0700

    Added fade amination to the feedback list

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import FeedbackItem from './FeedbackItem'
+import { motion, AnimatePresence } from 'framer-motion';
+import PropTypes from 'prop-types';
+import FeedbackItem from './FeedbackItem';
 
-function FeedbackList({ feedback, handleDelete}) {
-
-// the function check is there is feeback
+function FeedbackList({ feedback, handleDelete }) {
+  // the function check is there is feeback
   if (!feedback || feedback.length === 0) {
-    return <p>No Feedback!</p>
+    return <p>No Feedback!</p>;
   }
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
-  )
+  );
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+  //     ))}
+  //   </div>
+  // )
 }
 
 FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired
-  }))
-}
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
-
-export default FeedbackList
+export default FeedbackList;
