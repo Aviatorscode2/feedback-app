3fe4a68e085475a1c69ae1954c6b004bdd687a1b
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-25 15:23:10 -0700

    Create the add new Feedback function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/feedbackData';
@@ -8,6 +9,13 @@ import FeedbackForm from './components/FeedbackForm';
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  // Function to get add new feedback to the feed
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  // function to delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
@@ -17,7 +25,7 @@ const App = () => {
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
