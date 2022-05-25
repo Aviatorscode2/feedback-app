5ae192b17300e4b8dcfc3e2fc206eab8d5dc5efd
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-24 19:09:48 -0700

    Component for FeedbackStats

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React, {useState} from 'react'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/feedbackData'
+import React, { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/feedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 const App = () => {
-
   const [feedback, setFeedback] = useState(FeedbackData);
 
   const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id)) 
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-    
-  }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList
-          feedback={feedback}
-          handleDelete={deleteFeedback}
-        />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
-}
+};
 
-export default App
+export default App;
