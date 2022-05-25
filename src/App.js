4dcfe7cb3c4ddb7a27073d4aa6499a5b945828f6
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-25 07:41:06 -0700

    FeedbackForm state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/feedbackData';
 import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -16,6 +17,7 @@ const App = () => {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
