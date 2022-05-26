54cf40a7b05efa6da87ba8636183034d6fd50d05
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-26 16:45:31 -0700

    replaced ReactDOM with the new React 18 syntax

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import "./index.css";
+import { createRoot } from 'react-dom/client';
+// import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
