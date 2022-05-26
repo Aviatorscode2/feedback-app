a683ffda94a0bed1f7bc1ced1139bc2847741a94
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-26 14:59:03 -0700

    resolved the ReactDOM error on the console

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/feedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import About from './pages/About';
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -22,14 +24,29 @@ const App = () => {
     }
   };
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 };
 
