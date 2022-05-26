51eda83ffd7244277b62a613e8489650db1ff4bf
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-26 16:42:50 -0700

    Implemented an About page for our feedback app

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import Card from '../components/shared/Card';
+import { Link } from 'react-router-dom';
+function About() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>This is a React app to leave a feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+          <Link to="/">Back To Home</Link>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default About;
