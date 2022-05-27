38c9a5a4fc9cc93d538583e52f4c4b3596512455
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-27 04:20:15 +0100

    connected application to json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,35 +1,31 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      rating: 10,
-      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, itaque.',
-    },
-    {
-      id: 2,
-      rating: 9,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse voluptatem autem doloremque aliquam!',
-    },
-    {
-      id: 3,
-      rating: 7,
-      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, tempore error dolor nisi laudantium eligendi',
-    },
-    {
-      id: 4,
-      rating: 4,
-      text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, voluptate delectus.',
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  // useEffect for fetching feedback from json server
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // function to fetch feedback data from json server
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    );
+    const data = await response.json();
+
+    setFeedback(data);
+    isLoading(false);
+  };
 
   // Function to get add new feedback to the feed
   const addFeedback = (newFeedback) => {
@@ -69,6 +65,7 @@ export const FeedbackProvider = ({ children }) => {
         editFeedback,
         feedbackEdit,
         updateFeedback,
+        isLoading,
       }}
     >
       {children}
