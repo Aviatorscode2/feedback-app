bb2b643b35c3b23ea1026c197f01647a096af21d
Aviatorscode2 <victoryakubu5@gmail.com>
2022-05-27 03:13:07 +0100

    Function & Context to update an existing feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -6,15 +6,44 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, illo.',
+      rating: 10,
+      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, itaque.',
+    },
+    {
+      id: 2,
       rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse voluptatem autem doloremque aliquam!',
+    },
+    {
+      id: 3,
+      rating: 7,
+      text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, tempore error dolor nisi laudantium eligendi',
+    },
+    {
+      id: 4,
+      rating: 4,
+      text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel, voluptate delectus.',
     },
   ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
 
   // Function to get add new feedback to the feed
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
+  };
+
+  // Update feedback item
+  const updateFeedback = (id, updatedItem) => {
+    setFeedback(
+      feedback.map((item) =>
+        item.id === id ? { ...item, ...updatedItem } : item
+      )
+    );
   };
 
   // function to delete feedback
@@ -23,12 +52,23 @@ export const FeedbackProvider = ({ children }) => {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
+
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
+        feedbackEdit,
+        updateFeedback,
       }}
     >
       {children}
